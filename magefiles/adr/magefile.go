a00926d487c2734695fe3a866e1c733ca851f14f
Łukasz Bieszczad <l.bieszczad@clearcode.cc>
2021-08-16 12:38:18 +0200

    Have ADR tools in a docker image

--- magefiles/adr/magefile.go
+++ magefiles/adr/magefile.go
@@ -1,46 +1,103 @@
 package adr
 
 import (
+	"bytes"
+	"context"
 	"fmt"
 	"os"
+	"os/user"
 	"strings"
+	"text/template"
 
 	"github.com/magefile/mage/sh"
 )
 
-// Install installs adr tools in a version specified in a ADR_VERSION environment variable.
-// If the variable is not passed, latest version is used. To force updating the tool
-// use ADR_VERSION=latest.
-func Install() error {
-	var url string
-	version := os.Getenv("ADR_VERSION")
+const DOCKER_EXEC = ("docker run --rm -u {{.UID}}:{{.GID}} -v {{.AdrDir}}:/doc " +
+	"-v {{.AdrConfigDir}}:/adr-config -e ADR_TEMPLATE=/adr-config/{{.AdrTemplate}} " +
+	"brianskarda/adr-tools-docker adr {{.Cmd}}")
 
-	// URL with version must have "v" in the beginning
-	if version == "latest" {
-		url = "github.com/marouni/adr@latest"
-	} else {
-		url = fmt.Sprintf("github.com/marouni/adr@v%s", version)
-	}
+const (
+	adrDirDefault       = "documentation/adr/"
+	adrConfigDirDefault = ".adr-config/"
+	adrTemplateDefault  = "template.rst"
+)
 
-	// check exit code and version of adr tools
-	output, err := sh.Output("adr", "--version")
-	if sh.ExitStatus(err) != 0 || !strings.Contains(output, version) {
-		fmt.Println(
-			"adr tools not found or found in a different than requested version, installing...",
-		)
-		return sh.RunV("go", "install", url)
-	}
-
-	fmt.Println("adr tools already installed, skipping...")
-	return nil
+type params struct {
+	UID          string
+	GID          string
+	AdrDir       string
+	AdrConfigDir string
+	AdrTemplate  string
+	Cmd          string
 }
 
-// Init initializes adr in a given directory. Requires one argument - directory
-func Init(dir string) error {
-	return sh.RunV("adr", "init", dir)
+func buildParams(ctx context.Context, cmd, title string) (*params, error) {
+	var adrDir, adrConfigDir, adrTemplate interface{}
+
+	cmd = fmt.Sprintf("%s %s", cmd, title)
+	usr, err := user.Current()
+	if err != nil {
+		return nil, err
+	}
+	pwd, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+
+	if adrDir = ctx.Value("adrDir"); adrDir == nil {
+		adrDir = adrDirDefault
+	}
+	if adrConfigDir = ctx.Value("adrConfigDir"); adrConfigDir == nil {
+		adrConfigDir = adrConfigDirDefault
+	}
+	if adrTemplate = ctx.Value("adrTemplate"); adrTemplate == nil {
+		adrTemplate = adrTemplateDefault
+	}
+
+	return &params{
+		Cmd:          cmd,
+		AdrDir:       fmt.Sprintf("%s/%s", pwd, adrDir.(string)),
+		AdrConfigDir: fmt.Sprintf("%s/%s", pwd, adrConfigDir.(string)),
+		AdrTemplate:  adrTemplate.(string),
+		UID:          usr.Uid,
+		GID:          usr.Gid,
+	}, nil
 }
 
-// New creates a new adr entry file. Requires one argument - entry title
-func New(title string) error {
-	return sh.RunV("adr", "new", title)
+func buildCommand(ctx context.Context, cmd, path string) ([]string, error) {
+	tmpl, err := template.New("RUN_ADR").Parse(DOCKER_EXEC)
+	if err != nil {
+		return []string{}, err
+	}
+
+	params, err := buildParams(ctx, "init", path)
+	if err != nil {
+		return []string{}, err
+	}
+
+	buf := new(bytes.Buffer)
+	err = tmpl.Execute(buf, params)
+	if err != nil {
+		return []string{}, err
+	}
+
+	command := strings.Split(buf.String(), " ")
+	return command, nil
+
+}
+
+// New creates a new ADR entry. Specify title of an ADR entry.
+// By default adrDir, adrConfigDir, and adrTemplate are set.
+// If you wish to override them use ctx.WithValue() to
+// set corresponding values and pass the context to this function.
+//   * adrDir - directory which docker mounts as /docs
+//   * adrConfigDir - directory with configuration which docker mounts as /adr-config
+//   * adrTemplate - template name which will be used to produce ADRs.
+func New(ctx context.Context, title string) error {
+	var command []string
+	var err error
+	if command, err = buildCommand(ctx, "new", title); err != nil {
+		return err
+	}
+	return sh.Run(command[0], command[1:]...)
 }
